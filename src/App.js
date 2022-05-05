a28310f1d0072ec69c4892c2dd79db4c2f900bfe
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-05-06 01:17:52 +0400

    allows users to add phone numbers to the phone book

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName, 
+      number: newNumber
     }
     const currentPerson = persons.filter((person) => person.name === newName);
     if (currentPerson.length === 1) {
       alert(`${newName} is already added to phonebook`)
     } else {
     setPersons(persons.concat(nameObject))
-    setNewName('')  }
+    setNewName('')
+    setNewNumber('')  
+  }
   }
 
   const handleNameChange = (event) => {
@@ -25,12 +29,21 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
