22e0866da22194627b58de4e476dd5ddc0ab9874
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-05-06 01:38:46 +0400

    Implemented a search field that can be used to filter the list of people by name

--- src/App.js
+++ src/App.js
@@ -23,21 +23,27 @@ const Form = ({ addName, newName, handleNameChange, newNumber, handleNumberChang
   )
 }
 
-const Content = ({ person }) => {
+const Content = ({ personsFilter }) => {
   return (
     <ul>
-        {person.map(person =>
+        {personsFilter.map(person =>
           <Person key={person.name} person={person} />)}
       </ul>
   )
 }
 
 const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const[searchName, setNewSearch] = useState('')
+  const[personsFilter, setPersonsFilter] = useState(persons)
+ 
   
   const addName = (event) => {
     event.preventDefault()
@@ -49,7 +55,7 @@ const App = () => {
     if (currentPerson.length === 1) {
       alert(`${newName} is already added to phonebook`)
     } else {
-    setPersons(persons.concat(nameObject))
+    setPersonsFilter(personsFilter.concat(nameObject))
     setNewName('')
     setNewNumber('')  
     }
@@ -65,13 +71,38 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const filterPersons = (event) => {
+    //ამ წამს პერსონსფილტრი არის თვითონ პერსონ ერეის ტოლი
+    //ვქმნით ახალ ცვლადს, რომელსაც ვანიჭებთ ინპუთ ველში ჩაწერილი 
+    //სტრინგის მნიშვნელობას ოღონდ დაბალ რეგისტრში ჩაწერილს
+
+    const searchName = event.target.value.toLowerCase()
+
+    //ახალძებნას ვუტოლებთ ამ საძიებოსახელს
+    setNewSearch(searchName)
+
+    //შემოგვაქვს ახალი ცვლადი და ვამბობ ესეთ რამეს:
+    //ახლა მოცემულ პერსონების ერეის გავფილტრავ, და დავტოვებ მხოლოდ იმათ,
+    //რომლების სახელიც გადაყვანილი დაბალ რეგისტრში შეიცავენ საძიებოსახელს
+    const newPersons = persons.filter (
+      (person) => 
+        person.name.toLowerCase().search(searchName) !== -1
+    )
+
+    //ამის შემდეგ პერსონების ფილტრს ახალ პერსონებზე ვაყენებ
+    setPersonsFilter(newPersons)
+  }
+
   return (
     <div>
       <Heading heading='Phonebook'/>
+      <div>
+        filter shown with: <input value={searchName} onChange={filterPersons} />
+      </div>
       <Form addName={addName} newName={newName} newNumber={newNumber}
             handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <Heading heading='Numbers'/>
-      <Content person={persons} />
+      <Content personsFilter={personsFilter} />
     </div>
   )
 }
