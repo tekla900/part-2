76321ffde47036f4f925050c0834c493564aa049
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-05-06 01:24:52 +0400

    divided the main App component into Header, Form and Content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import { useState } from 'react'
 import Person from './components/Person'
 
+const Heading = ({ heading }) => {
+  return (
+    <h2>{heading}</h2>
+  )
+}
+
+const Form = ({ addName, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (
+    <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+
+const Content = ({ person }) => {
+  return (
+    <ul>
+        {person.map(person =>
+          <Person key={person.name} person={person} />)}
+      </ul>
+  )
+}
+
 const App = () => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-
+  
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
@@ -21,7 +52,7 @@ const App = () => {
     setPersons(persons.concat(nameObject))
     setNewName('')
     setNewNumber('')  
-  }
+    }
   }
 
   const handleNameChange = (event) => {
@@ -34,26 +65,13 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <Person key={person.name} person={person} />)}
-      </ul>
+      <Heading heading='Phonebook'/>
+      <Form addName={addName} newName={newName} newNumber={newNumber}
+            handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
+      <Heading heading='Numbers'/>
+      <Content person={persons} />
     </div>
   )
 }
