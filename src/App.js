80141a31e24372039366b255f64123208dfb5b84
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-05-06 16:44:34 +0400

    The initial state of the data is fetched from the server (http://localhost:3001/persons) using axios-library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Form from './components/Form'
-import Header from './Header' 
+import Header from './components/Header' 
 import Content from './components/Content'
 import Search from './components/Search'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const[searchName, setNewSearch] = useState('')
   const[personsFilter, setPersonsFilter] = useState(persons)
  
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersonsFilter(response.data)
+      })
+  }
   
+  useEffect(hook, [])
+
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
